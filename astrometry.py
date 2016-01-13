cfc6c4e05820268f2610578fb595e6d72b527415
ariedel <adric.riedel@gmail.com>
2016-01-13 10:02:45 -0800

    wstddev now expects uncertainties, not weights.

--- astrometry.py
+++ astrometry.py
@@ -1,7 +1,8 @@
 import numpy
 
 # Weighted Standard Devation taken from http://stackoverflow.com/questions/2413522/weighted-standard-deviation-in-numpy
-def wstddev(x,w):
+def wstddev(x,u):
+    w = 1/(u**2)
     average = numpy.average(x,weights=w)
     variance = numpy.dot(w,(x-average)**2)/w.sum()
     return average,numpy.sqrt(variance)
